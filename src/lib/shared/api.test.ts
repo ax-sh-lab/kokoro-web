e033e7609fa55cfd77313370749d870cce088800
ax-sh <56112475+ax-sh@users.noreply.github.com>
2026-01-04 11:54:49 +0545

    chore: update vitest

--- src/lib/shared/api.test.ts
+++ src/lib/shared/api.test.ts
@@ -3,31 +3,27 @@ import path from "path";
 import OpenAI from "openai";
 import { describe, it } from "vitest";
 
-describe(
-  "test api",
-  () => {
-    it("should ", async () => {
-      const host = "http://localhost:8880";
-      const openai = new OpenAI({
-        baseURL: host + "/api/v1",
-        apiKey: "your-kokoro-api-key",
-      });
-      const speechFile = path.resolve("./speech.mp3");
-      try {
-        const mp3 = await openai.audio.speech.create({
-          model: "model_q8f16",
-          // @ts-ignore
-          voice: "af_heart",
-          input: "Today is a wonderful day to build something people love!",
-        });
-        console.log("[success]", mp3);
-
-        const buffer = Buffer.from(await mp3.arrayBuffer());
-        await fs.promises.writeFile(speechFile, buffer);
-      } catch (e) {
-        console.dir(e);
-      }
+describe("test api", { timeout: 60000 }, () => {
+  it("should ", async () => {
+    const host = "http://localhost:8880";
+    const openai = new OpenAI({
+      baseURL: host + "/api/v1",
+      apiKey: "your-kokoro-api-key",
     });
-  },
-  { timeout: 6000000 },
-);
+    const speechFile = path.resolve("./speech.mp3");
+    try {
+      const mp3 = await openai.audio.speech.create({
+        model: "model_q8f16",
+        // @ts-ignore
+        voice: "af_heart",
+        input: "Today is a wonderful day to build something people love!",
+      });
+      console.log("[success]", mp3);
+
+      const buffer = Buffer.from(await mp3.arrayBuffer());
+      await fs.promises.writeFile(speechFile, buffer);
+    } catch (e) {
+      console.dir(e);
+    }
+  });
+});
